a7c1dc28980450ea9317f670de53dfa6964d4445
NotTheJerry <quinterodiego208@gmail.com>
2024-01-01 02:03:34 -0600

    Refactorizando los directorios para tener las carpetas del front y back de la seccion de teoria en un mismo repo

---
+++ fullstack-teoria-frontend/src/services/notes.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = 'https://fullstacopen-app-backend.onrender.com/api/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+
+export default { getAll, create, update }
