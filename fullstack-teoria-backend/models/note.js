976a96e0bb118fb67aa4bdd8ac939a4358f44d88
NotTheJerry <quinterodiego208@gmail.com>
2024-01-01 18:16:49 -0600

    Terminado una seccion de la parte 4.a "Estructura de la aplicacion backend", le di estructura al backend del proyecto de teoria

--- fullstack-teoria-backend/models/note.js
+++ fullstack-teoria-backend/models/note.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
