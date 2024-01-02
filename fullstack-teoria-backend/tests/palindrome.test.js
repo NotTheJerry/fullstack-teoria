6656fa346ab1c82d895adaf865d77419c1dc25a4
NotTheJerry <quinterodiego208@gmail.com>
2024-01-02 14:08:40 -0600

    Terminando toda la teoria de la parte 4.a "Estructura de la aplicacion backend, introduccion a las pruebas"

---
+++ fullstack-teoria-backend/tests/palindrome.test.js
@@ -0,0 +1,35 @@
+const palindrome = require('../utils/for_testing').palindrome
+
+test('palindrome of a', () => {
+  const result = palindrome('a')
+
+  expect(result).toBe('a')
+})
+
+test('palindrome of react', () => {
+  const result = palindrome('react')
+
+  expect(result).toBe('tcaer')
+})
+
+test('palindrome of relever', () => {
+  const result = palindrome('releveler')
+
+  expect(result).toBe('releveler')
+})
+
+const average = require('../utils/for_testing').average
+
+describe('average', () => {
+  test('of one value is the value itself', () => {
+    expect(average([1])).toBe(1)
+  })
+
+  test('of many is calculated right', () => {
+    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
+  })
+
+  test('of empty array is zero', () => {
+    expect(average([])).toBe(0)
+  })
+})
